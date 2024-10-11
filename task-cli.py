aa8f92c4e4fb4f533d66932d966cb6b6e05bf1ec
trandonbay <brandontay95@gmail.com>
2024-10-11 15:40:14 -0700

    Finished functionality

--- task-cli.py
+++ task-cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import json
+import datetime
+import sys
 
 def load_tasks(file_name="tasks.json"):
     try:
@@ -14,38 +16,76 @@ def save_tasks(tasks, file_name="tasks.json"):
     with open(file_name, "w") as f:
         json.dump(tasks, f, indent=4)
 
-def add_task(tasks):
-    description = input("Enter task description: ")
-    task = {"id": len(tasks) + 1, "description": description, "status": "In Progress"}
+def add_task(tasks, description):
+    timestamp = datetime.datetime.now().isoformat()
+    task = {
+        "id": len(tasks) + 1, 
+        "description": description, 
+        "status": "In Progress",
+        "createdAt": timestamp,
+        "updatedAt": timestamp}
     tasks.append(task)
 
-def view_tasks(tasks):
+def update_task(tasks, task_id, description):
     for task in tasks:
-        print(f'{task["id"]}: {task["description"]} [{task["status"]}]')
+        if task["id"] == task_id:
+            task["description"] = description
+            task["updatedAt"] = datetime.datetime.now().isoformat()
 
-# def update_task(tasks):
-# def delete_task(tasks):
+def update_status(tasks, task_id, status):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = status
+        
+def delete_task(tasks, task_id):
+    tasks[:] = [task for task in tasks if task["id"] != task_id]
+
+def list_tasks(tasks, status=None):
+    if not tasks:
+        print("There are currently no tasks. Please add a task.")
+
+    if status == "in-progress":
+        for task in tasks:
+            if task["status"] == "In Progress":
+                print(f'{task["id"]}: {task["description"]} [{task["status"]}]')
+    elif status == "done":
+        for task in tasks:
+            if task["status"] == "Done":
+                print(f'{task["id"]}: {task["description"]} [{task["status"]}]')
+    else:
+        for task in tasks:
+            print(f'{task["id"]}: {task["description"]} [{task["status"]}]')
 
 def main():
     tasks = load_tasks()
 
-    while True:
-        print("\nTask Tracker")
-        print("[add] Add a task")
-        print("[view] View all tasks")
-        print("[quit] Save")
+    args = sys.argv
 
-        choice = input("Enter choice: ")
-
-        if choice.lower() == "add":
-            add_task(tasks)
-        elif choice.lower() == "view":
-            view_tasks(tasks)
-        elif choice.lower() == "quit":
-            save_tasks(tasks)
-            break
+    try:
+        if args[1].lower() == "add":
+            description = args[2]
+            add_task(tasks, description)
+        elif args[1].lower() == "update":
+            task_id, new_description = int(args[2]), args[3]
+            update_task(tasks, task_id, new_description)
+        elif args[1].lower() == "status":
+            task_id, new_status = int(args[2]), args[3]
+            update_status(tasks, task_id, new_status)
+        elif args[1].lower() == "delete":
+            task_id = int(args[2])
+            delete_task(tasks, task_id)
+        elif args[1].lower() == "list":
+            try:
+                status = args[2]
+                list_tasks(tasks, status)
+            except IndexError:
+                list_tasks(tasks)
         else:
-            print("Invalid choice. Please try again.")
+            print("Please try again.")
+    except IndexError:
+        print("Needs more details.")
+
+    save_tasks(tasks)
 
 if __name__ == "__main__":
     main()
